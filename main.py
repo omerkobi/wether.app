ac4017e6dd5484bf0f588db8e4976a91ecbde07d
omerkobi <omerkobi9@gmail.com>
2024-12-22 19:53:37 +0200

    Update main.py
    added streamlit

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 print('hello')
+print('say hello my friend')
+import streamlit as st
+st.markdown('''
+#Title 
+##subtitle
+
+-bullet1
+-bullet2
+bullet3
+
+> Amazingv quote :
+''')
+
+st.radio("Which desert is better?", ["cake","ice cream","Pie"])
+
