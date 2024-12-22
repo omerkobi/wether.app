913bcf2400736a06a2a43b502467ff068dc67df5
omerkobi <omerkobi9@gmail.com>
2024-12-22 20:34:34 +0200

    Update main.py
    api + json

--- main.py
+++ main.py
@@ -1,16 +1,47 @@
+import streamlit as st
+import requests as re
+
 print('hello')
 print('say hello my friend')
-import streamlit as st
-st.markdown('''
+######### streamlit ###########
+#st.markdown('''
 #Title 
 ##subtitle
 
--bullet1
--bullet2
-bullet3
+#-bullet1
+#-bullet2
+#bullet3
 
-> Amazingv quote :
-''')
+#> Amazingv quote :
+#''')
 
-st.radio("Which desert is better?", ["cake","ice cream","Pie"])
+#st.radio("Which desert is better?", ["cake","ice cream","Pie"])
+
+################## API ##################
+categories_url = 'https://api.chucknorris.io/jokes/categories'
+response = re.get(categories_url)
+response.raise_for_status() # 200 ok
+print(response.text)
+
+########## Using jason ##########
+import  json
+categories = json.loads(response.text)
+print(type(categories))
+
+#### Shorter way using json ######
+print(response.json())
+print(response.json()[1])
+
+
+def get_chuck_norris_joke_by_category(category):
+    url = f"https://api.chucknorris.io/jokes/random?category={category}"
+    response = re.get(url)
+    response.raise_for_status()
+    return response.json()
+
+
+# Try categories like 'animal', 'career', 'celebrity'
+joke_dict = get_chuck_norris_joke_by_category('animal')
+# print(joke_dict)
+print(joke_dict['value'])
 
